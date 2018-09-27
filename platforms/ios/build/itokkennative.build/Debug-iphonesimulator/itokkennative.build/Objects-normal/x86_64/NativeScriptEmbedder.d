8668d18774dac1031f47ff0cc23788666dc7b3c8
AshwinKushwah <ashwinkushwah00@gmail.com>
2018-09-27 22:00:17 +0530

    creating itokken NativeScript App

---
+++ platforms/ios/build/itokkennative.build/Debug-iphonesimulator/itokkennative.build/Objects-normal/x86_64/NativeScriptEmbedder.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/kushwah/Desktop/itokken-native/platforms/ios/itokkennative/itokkennative-Prefix.pch \
+  /Users/kushwah/Desktop/itokken-native/node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.m \
+  /Users/kushwah/Desktop/itokken-native/platforms/ios/../../node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.h
---
+++ platforms/ios/build/itokkennative.build/Debug-iphonesimulator/itokkennative.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/kushwah/Desktop/itokken-native/platforms/ios/itokkennative/itokkennative-Prefix.pch \
+  /Users/kushwah/Desktop/itokken-native/platforms/ios/internal/main.m \
+  /Users/kushwah/Desktop/itokken-native/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/kushwah/Desktop/itokken-native/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/kushwah/Desktop/itokken-native/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/kushwah/Desktop/itokken-native/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/kushwah/Desktop/itokken-native/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/kushwah/Desktop/itokken-native/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/kushwah/Desktop/itokken-native/platforms/ios/internal/TNSDebugging.h \
+  /Users/kushwah/Desktop/itokken-native/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
